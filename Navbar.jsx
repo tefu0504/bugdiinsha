2e95c171f5175f67ececf0a2d23a0049e29f1bf9
tefu0504 <tefutoefu@gmail.com>
2025-07-27 11:08:41 +0800

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase-config";
 
-function Navbar({ user, cart }) {
+function Navbar({ user, setUser, cart }) {
+  const handleLogout = async () => {
+    await signOut(auth);
+    setUser(null);
+  };
+
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between">
       <div>
@@ -11,8 +18,14 @@ function Navbar({ user, cart }) {
       <div>
         {user ? (
           <>
-            <span className="mr-4">Hi, {user.username}</span>
+            <span className="mr-4">Hi, {user.email}</span>
             <Link to="/account" className="mr-4 hover:underline">Account</Link>
+            <button
+              onClick={handleLogout}
+              className="bg-red-600 px-3 py-1 rounded hover:bg-red-700"
+            >
+              Logout
+            </button>
           </>
         ) : (
           <>
